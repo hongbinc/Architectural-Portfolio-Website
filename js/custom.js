8bceaac9f4cccedd06f3f5f03b355a065f8824d6
hongbinc <hongbin1992@hotmail.com>
2015-09-18 20:45:42 -0400

    Adding new repository

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+﻿//==== Beginning animation =======
+$(document).ready(function () {
+   
+    var div = $("#wel");
+    var pi = $("#pic1");
+    div.css("animation", "changeopacity 3s")
+    
+    function next() {
+        var pi = $("#pic1"); 
+        div.remove();
+        pi.css("opacity", 1); //set Opacity of img to 1
+    }
+    setTimeout(next, 3000); // After 3s remove "welcome"
+    
+});
