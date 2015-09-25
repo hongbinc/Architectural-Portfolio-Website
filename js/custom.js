964ddb3432e29403f62a4b3c95d61e2c5cd6a705
hongbinc <hongbin1992@hotmail.com>
2015-09-25 14:05:30 -0400

    updata

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,14 @@ $(document).ready(function () {
    
     var div = $("#wel");
     var pi = $("#pic1");
+    pi.hide();
     div.css("animation", "changeopacity 3s")
-    
+
     function next() {
         var pi = $("#pic1"); 
-        div.remove();
-        pi.css("opacity", 1); //set Opacity of img to 1
+        div.remove(); // Remove 'welcome'
+        pi.show();    // Show the image after remove 'welcome'
     }
-    setTimeout(next, 3000); // After 3s remove "welcome"
+    setTimeout(next, 3000); // After 3s remove "welcome",then run next()
     
 });
--- js/custom2.js
+++ js/custom2.js
@@ -1,4 +1,4 @@
-﻿// ===== Scroll to Top ==== 
+﻿// ===== Scroll to Top ==== //
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
